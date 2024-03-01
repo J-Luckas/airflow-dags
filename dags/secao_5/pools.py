a4e04dcb08542b125762f03395e65d3c288866da
J-Luckas <jairoluckas@hotmail.com>
2024-03-01 14:11:33 -0300

    feat: change env

---
+++ dags/secao_5/pools.py
@@ -0,0 +1,24 @@
+from airflow import DAG
+from datetime import datetime
+from airflow.operators.bash_operator import BashOperator
+
+
+dag = DAG("pool", 
+          description="pool", 
+          schedule_interval=None, 
+          start_date=datetime(2024,2,20), 
+          catchup=False
+)
+
+
+
+task1 = BashOperator(task_id="tsk1", bash_command='sleep 5' , dag=dag)
+task2 = BashOperator(task_id="tsk2", bash_command='sleep 5' , dag=dag)
+task3 = BashOperator(task_id="tsk3", bash_command='sleep 5' , dag=dag)
+task4 = BashOperator(task_id="tsk4", bash_command='sleep 5' , dag=dag)
+
+
+
+
+
+
